af7be3c2819fd627f14d4279f5a2f1a00afefb0f
lilianecardinot <>
2022-07-27 19:06:29 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.getElementById("botãoEnviar").addEventListener("click")
+
+
+if  (document.getElementById("nome").value != "" && document.getElementById("email").value != ""){
+  alert("Prontinho! Você receberá as novidades por email.")
+}else{
+  alert("Por favor, preencha os campos nome e email!")
+}
